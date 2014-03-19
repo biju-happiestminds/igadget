e1c666bca8367c12feb7605a29dbebbdaa565f33
Biju Parvathy <biju.parvathy@happiestminds.com>
2014-03-19 15:33:58 +0530

    -

---
+++ scripts/pages/product.ts
@@ -0,0 +1,30 @@
+$("./body") {
+	add_class("ig_details")
+	
+	$(".//div[@id='SideNewProducts']") {
+		remove()
+	}
+	
+	$(".//div[@id='SideNewsletterBox']") {
+		remove()
+	}
+	
+	
+	$(".//div[@class='Block Moveable Panel PrimaryProductDetails']") {
+		add_class("ig_PrimaryProductDetails")
+	}
+	
+	$$("#ProductDescription") {
+		attribute("data-ur-set", "toggler")
+		attribute("class","mw_menu_btn")
+		$("h3") {
+			attribute("data-ur-toggler-component", "button")
+		}
+		$(".//div[@class='ProductDescriptionContainer']") {
+			attribute("data-ur-toggler-component", "content")
+		}
+		$("./ProductDescriptionContainer") {
+			attribute("data-ur-toggler-component", "content")
+		}
+	}
+}
